938aeb8b721e695a1c88ea16ce5d2893304391cf
David Alvarez <hola@alvarezdavid.com>
2020-10-13 05:34:53 +0000

    Adding functions about branch

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
@@ -25,4 +27,63 @@ func main() {
 		return nil
 	})
 
+	//Return all branch name
+	branchesName, err := getBranches(".")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Branches")
+		for i, s := range branchesName {
+			fmt.Println(i, s)
+		}
+	}
+
+	//Return if exist a specific branch
+	branchName := "devnotes2"
+	result, _ := getBranch(".", branchName)
+	if result {
+		fmt.Println("Exist " + branchName)
+	}
+}
+
+//Return all branches specific repository
+// If only have one branch "master" dont return it
+func getBranches(repoPath string) ([]string, error) {
+	repo, err := git.PlainOpen(repoPath)
+	const BranchPrefix = "refs/heads/"
+	var branchNames []string
+	if err != nil {
+		return nil, err
+	}
+
+	if repo != nil {
+		branches, _ := repo.Branches()
+
+		if branches != nil {
+
+			_ = branches.ForEach(func(branch *plumbing.Reference) error {
+				branchNames = append(branchNames, strings.TrimPrefix(branch.Name().String(), BranchPrefix))
+				return nil
+			})
+
+			return branchNames, err
+		}
+	}
+
+	return branchNames, nil
+}
+
+//Verify if a branch exist
+func getBranch(repoPath string, branchName string) (bool, error) {
+	const BranchPrefix = "refs/heads/"
+	repo, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return false, err
+	}
+	_, err = repo.Reference(plumbing.ReferenceName(BranchPrefix+branchName), true)
+	if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
 }
